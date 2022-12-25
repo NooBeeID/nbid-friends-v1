f085a170287d96564b36468355b51be1665ee2f8
Reyhan Jovie Dwiputra <reyhanjovie01@gmail.com>
2022-12-25 08:38:21 +0700

    update cors

--- apps/router/gin.go
+++ apps/router/gin.go
@@ -43,10 +43,10 @@ func (r *Gin) BuildRoutes() {
 }
 
 func CORS(ctx *gin.Context) {
-	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Credentials", "true")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	ctx.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 	if ctx.Request.Method == "OPTIONS" {
 		ctx.AbortWithStatus(204)
